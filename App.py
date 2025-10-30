1b8eb6be3860398c49e9de4233af3280f91eb992
Yoseba <yra0005@alu.medac.es>
2025-10-30 14:26:59 +0100

    segundo commit (modificar nombre)

--- App.py
+++ App.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return "Hola Mundo"
+    return "Hola Mundo 1"
+
+    # segundo commit añado un 1
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
